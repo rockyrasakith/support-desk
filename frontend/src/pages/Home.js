44de896de49977665b033dd7280516ea23407dd2
Rocky Rasakith <rocky.rasakith@saile.ai>
2022-08-08 13:57:56 +0200

    remove react-typical npm package

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,42 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaQuestionCircle } from "react-icons/fa";
 import { ImEye } from "react-icons/im";
-import Typical from "react-typical";
-import { useSelector } from "react-redux";
 
 const Home = () => {
-  const { user } = useSelector((state) => state.auth);
+  
   return (
     <>
       <section className="heading">
         <h1>Which programming language do you need help with?</h1>
-        {!user && (
-          <div
-            style={{
-              fontFamily: "courier new",
-              margin: "20px",
-              fontSize: "1.2em",
-              fontWeight: "bold",
-              color: "#e74c3c",
-            }}
-          >
-            <Typical
-              steps={[
-                "<html> Hello World </html>",
-                2000,
-                `css {hello: 
-            world} `,
-                2000,
-                `JavaScript.log("Hello World")`,
-                2000,
-              ]}
-              loop={3}
-              wrapper="p"
-            />
-          </div>
-        )}
       </section>
-      {user && (
+     
         <>
           <Link
             to="/new-question"
@@ -54,7 +27,7 @@ const Home = () => {
             <ImEye /> View my questions
           </Link>
         </>
-      )}
+  
     </>
   );
 };
