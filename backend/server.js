4e116bf0e7f66f6237d8acc47676a788f2645bbb
Rocky Rasakith <rocky.rasakith@saile.ai>
2022-08-08 13:35:59 +0200

    ready for live deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8000;
 const colors = require("colors");
 const connectDB = require("./config/db");
+const path = require("path")
 
 // Connect to the database
 connectDB();
@@ -12,15 +13,26 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Code Support API" });
-});
-
 // Route for user controls
 app.use("/api/users", require("./routes/userRoutes"));
 
 //Route for question controls
 app.use("/api/questions", require("./routes/questionRoutes"))
+
+// Serve frontend
+if(process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Code Support API" });
+  });
+}
+
 
 app.use(errorHandler);
 
